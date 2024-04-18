3621da5ee6aeed91f71f6362cfe5b1792bb74378
Alton Maseko <altonmaseko1000@gmail.com>
2024-04-18 18:15:22 +0200

    final attempt to get azure site working :)

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.use(cors())
 app.use(cookieParser())
 app.use(express.json())
 
-app.use(express.static("./frontend"))
+app.use(express.static("./frontend")) //serve the front end
 
 // Dont need an access token to do these:
 app.use("/register", registerRouter)
