0ac9a60941d263e084187cefebd509808907bae4
Alton Maseko <altonmaseko1000@gmail.com>
2024-04-20 20:41:26 +0200

    front end headache 2

--- config/connectDB.js
+++ config/connectDB.js
@@ -3,9 +3,7 @@ const mongoose = require("mongoose")
 const connectDB = async () => {
     try {
         // await mongoose.connect("mongodb://localhost:27017/FundingWebsite") //TEMPORARY LOCAL DATABASE
-        
-        await mongoose.connect("mongodb://localhost:27017/FundingWebsite") //TEMPORARY LOCAL DATABASE
-        // await mongoose.connect(process.env.CONNECTION_URI)
+        await mongoose.connect(process.env.CONNECTION_URI)
     } catch (error) {
         console.log(error)
     }
