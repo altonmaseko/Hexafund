75784c35246a3c6adc3b59efa6b5ec0f13856de9
Banzile <banzile.nhlebela74@gmail.com>
2024-04-21 19:01:19 +0200

    trying something

--- config/connectDB.js
+++ config/connectDB.js
@@ -2,8 +2,8 @@ const mongoose = require("mongoose")
 
 const connectDB = async () => {
     try {
-        // await mongoose.connect("mongodb://localhost:27017/FundingWebsite") //TEMPORARY LOCAL DATABASE
-        await mongoose.connect(process.env.CONNECTION_URI)
+        await mongoose.connect("mongodb://localhost:27017/FundingRequestsManagement") //TEMPORARY LOCAL DATABASE
+        // await mongoose.connect(process.env.CONNECTION_URI)
     } catch (error) {
         console.log(error)
     }
