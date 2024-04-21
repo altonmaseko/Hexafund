034f74886995678fbb5e3a3f5a945f0e5e90e703
lucianIrsigler <l.irsigler64@gmail.com>
2024-04-21 16:56:56 +0200

    readded playwright

--- tests/e2e/example.spec.js
+++ tests/e2e/example.spec.js
@@ -1,23 +1,6 @@
 // @ts-check
 const { test, expect } = require('@playwright/test');
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
-});
-
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
-
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
-});
-
 test.beforeEach(async ({ page }) => {
   await page.goto('https://demo.playwright.dev/todomvc');
 });
@@ -464,4 +447,3 @@ async function checkTodosInLocalStorage(page, title) {
     return JSON.parse(localStorage['react-todos']).map(i => i.title).includes(t);
   }, title);
 }
-
