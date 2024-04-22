31d7595c779cf44f84115e8dc9643ff472e84a3c
lucianIrsigler <l.irsigler64@gmail.com>
2024-04-22 23:20:40 +0200

    updating workflow{

--- config/connectDB.js
+++ config/connectDB.js
@@ -4,7 +4,7 @@ const connectDB = async () => {
     try {
         if (process.env.NODE_ENV === "development") {
             let connectionURI = "";
-            if (process.env.MONGODB_ROOT_PASSWORD) {
+            if (process.env.CI) {
                 connectionURI = `mongodb://root:${process.env.MONGODB_ROOT_PASSWORD}@${process.env.MONGODB_HOST}:${process.env.MONGODB_PORT}/${process.env.MONGODB_DATABASE}`;
             }else{
                 connectionURI = "mongodb://localhost:27017/FundingRequestsManagement"
