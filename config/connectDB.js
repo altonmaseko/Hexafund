11438d827dd290eb1476a530d05e8a55babe93d2
Alton Maseko <altonmaseko1000@gmail.com>
2024-04-23 08:22:47 +0200

    fixed connecition uri

--- config/connectDB.js
+++ config/connectDB.js
@@ -2,16 +2,18 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
     try {
-        if (process.env.NODE_ENV === "development") {
-            let connectionURI = "";
-            if (process.env.CI) {
-                connectionURI = `mongodb://127.0.0.1:27017/FundingRequestsManagement`;
-            }else{
-                connectionURI = "mongodb://localhost:27017/FundingRequestsManagement"
-            }
-            //mongodb://127.0.0.1:27017
-            await mongoose.connect(connectionURI);
-        }
+        // ***this code doesnt work with azure
+        // if (process.env.NODE_ENV === "development") {
+        //     let connectionURI = "";
+        //     if (process.env.CI) {
+        //         connectionURI = `mongodb://127.0.0.1:27017/FundingRequestsManagement`;
+        //     }else{
+        //         connectionURI = "mongodb://localhost:27017/FundingRequestsManagement"
+        //     }
+        //     //mongodb://127.0.0.1:27017
+        //     await mongoose.connect(connectionURI);
+        // }
+        await mongoose.connect(process.env.CONNECTION_URI)
     } catch (error) {
         console.log(error);
     }
