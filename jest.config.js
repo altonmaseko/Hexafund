47876dddc5406a995dfdc28d99d2d48513d8adbf
lucianIrsigler <l.irsigler64@gmail.com>
2024-05-01 09:52:42 +0200

    added jest test coverage to workflow file

--- jest.config.js
+++ jest.config.js
@@ -4,7 +4,6 @@
 module.exports = {
     // Specifies the root directory for Jest to start searching for tests
     rootDir: './tests/jest-tests',
-    /*"collectCoverage": true,
-    "coverageDirectory": "./coverage",
-    "coverageReporters": ["text"],*/
+    "coverageDirectory": "../../coverage",
+    "coverageReporters": ["lcov"]
 };
