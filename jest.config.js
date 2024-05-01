f40601f20dd375f091b39e93fb7f89a4c41ed1ed
lucianIrsigler <l.irsigler64@gmail.com>
2024-05-01 09:17:33 +0200

    code coverage playwright

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,10 @@
 // jest.config.js
 
+
 module.exports = {
     // Specifies the root directory for Jest to start searching for tests
     rootDir: './tests/jest-tests',
+    /*"collectCoverage": true,
+    "coverageDirectory": "./coverage",
+    "coverageReporters": ["text"],*/
 };
