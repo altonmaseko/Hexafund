10ff4f81cb06b1f8e099c35fc69d2a1235ad96c0
Alton Maseko <altonmaseko1000@gmail.com>
2024-05-02 14:40:44 +0200

    dynamic and approvalDenyFundingOpportunities

--- models/FundingOpportunity.js
+++ models/FundingOpportunity.js
@@ -28,11 +28,13 @@ const FundingOpportunitySchema = new mongoose.Schema({
         type: mongoose.Schema.Types.ObjectId, // Use ObjectId type
         index: true, // Optional: Add an index for faster lookups
         auto: true // Automatically generate an ObjectId
-    }
-
-    /*
+    },
     // Features to be added later
     funding_amount: {
+        type: Number,
+        required: true
+    },
+    available_slots: { //the amount of people who will recieve it. IF budget = R10000, and funding_amount = R1000, then count = 10
         type: Number,
         required: true
     },
@@ -44,8 +46,12 @@ const FundingOpportunitySchema = new mongoose.Schema({
         type: String,
         enum: [statuses.ACTIVE, statuses.INACTIVE],
         default: statuses.ACTIVE
+    },
+    description: {
+        type: String,
+        default: "Apply for this funding Opportunity now! And start your journey of SUCCESS! You. Can. Do It."
     }
-    */
+    
     //funding_amount history will be added later
 });
 
